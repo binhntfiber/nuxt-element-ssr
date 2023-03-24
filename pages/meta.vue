4f9ee360c462c9903cc49e8dbdcd8b4c1d76319a
Binh Nguyen <nguyenthaibinh0131@gmail.com>
2023-03-24 20:32:32 +0700

    feat: add-meta page

---
+++ pages/meta.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const title = 'My Collections';
+const desc = 'Create, curate, and manage collections of unique NFTs to share and sell.';
+
+useHead({
+	title,
+	titleTemplate: '%s - Artrific V2',
+});
+
+useServerSeoMeta({
+	ogTitle: title,
+	description: desc,
+	ogDescription: desc,
+	twitterTitle: title,
+});
+</script>
+
+<template>
+	<div>Test meta</div>
+</template>
